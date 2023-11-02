47485038e40bbc017ea92e3d466a19b1f497ee19
Steve Korshakov <steve@korshakov.com>
2023-11-01 22:07:18 -0700

    wip: working on base UI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,30 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css'
 import { Button } from '../components/ui/button'
 import { ModeToggle } from '../components/mode-toggle'
+import { cn } from '@/lib/utils';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <ModeToggle />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR!
+    <div className={styles['app-box']}>
+      <div className={cn(styles['app-box-inner'])}>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <ModeToggle />
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR!
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
