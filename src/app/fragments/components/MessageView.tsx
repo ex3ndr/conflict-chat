5c3e2209054bc9034e9b9a5f92d5f14260f68c8a
Steve Korshakov <steve@korshakov.com>
2023-11-07 10:29:15 -0800

    wip: improving chat ui

--- src/app/fragments/components/MessageView.tsx
+++ src/app/fragments/components/MessageView.tsx
@@ -1,4 +1,5 @@
 import { MessageType } from '@/app/api/useMessages';
+import { cn } from '@/lib/utils';
 import * as React from 'react';
 
 export const MessageView = React.memo((props: { message: MessageType, me: string, opponent: string }) => {
@@ -7,14 +8,22 @@ export const MessageView = React.memo((props: { message: MessageType, me: string
     if (props.message.content.sender === 'incoming') {
         name = props.opponent;
     } else if (props.message.content.sender === 'outgoing') {
-        name = "You";
+        name = props.me + ' (You)';
+    } else {
+        console.warn('Unknown sender', props.message.content.sender);
+    }
+    let color = 'color-neutral-900';
+    if (props.message.content.sender === 'system') {
+        color = 'text-orange-600';
+    } else if (props.message.content.sender === 'incoming') {
+        color = 'text-blue-600';
     }
 
     return (
         <div className='flex flex-row px-[32px] py-[16px]'>
-            <div className='w-[36px] h-[36px] rounded-[32px] bg-black mr-[16px]' />
+            <div className='w-[36px] h-[36px] rounded-[32px] mr-[16px] flex-shrink-0 bg-accent mt-[8px]' />
             <div className='flex flex-col'>
-                <span>{name}</span>
+                <span className={cn('text-base text-neutral-900 font-semibold', color)}>{name}</span>
                 <span>{props.message.content.body.value}</span>
             </div>
         </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import styles from './header.module.css';
+import { ModeToggle } from './mode-toggle';
 
 export const Header = React.memo((props: { title: string, loading?: boolean }) => {
     return (
         <div className={styles.header}>
             <span className={styles.title}>{props.title}</span>
+            <ModeToggle />
         </div>
     )
 });
